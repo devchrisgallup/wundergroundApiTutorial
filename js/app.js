f1a7a61ab683b07f3e931365b2a1b5384cabc1a6
devchrisgallup <chris.gallup@hotmail.com>
2017-01-01 15:17:14 -0600

    update styles

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 // process request
 function getWeather() { 
-    console.log('works');
+    // create XMLHttpRequest object
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
+    // readyState 4 means the request is complete
+    // status 200 means was successful
     if (this.readyState == 4 && this.status == 200) {
+        // output div
         output = document.getElementById('weather');
+        // construct object
         weatherData = JSON.parse(this.responseText);
-        output.innerHTML = weatherData.current_observation.temp_f;
+        console.log(weatherData);
+        // place object key/value into div
+        output.innerHTML = weatherData.current_observation.temp_f + "&deg;";
     }
     };
-    xhttp.open("GET", "http://api.wunderground.com/api/yourkey/geolookup/conditions/q/IA/Cedar_Rapids.json", true);
+    // calls the open 
+    xhttp.open("GET", "http://api.wunderground.com/api/YouKey/geolookup/conditions/q/IA/Cedar_Rapids.json", true);
     xhttp.send();
 }
 // eventlisteners 
