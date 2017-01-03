d8bda04ba3cc5fe1c279dfacc5a1accc92a2b560
devchrisgallup <chris.gallup@hotmail.com>
2017-01-02 18:26:32 -0600

    add location div

--- js/app.js
+++ js/app.js
@@ -1,23 +1,50 @@
-// process request
-function getWeather() { 
+// start up 
+function startUp() {
+    fetchData();
+    hideText();
+}
+
+// Ajax call to wunderground's API
+function fetchData() {
     // create XMLHttpRequest object
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     // readyState 4 means the request is complete
     // status 200 means was successful
     if (this.readyState == 4 && this.status == 200) {
-        // output div
-        output = document.getElementById('weather');
         // construct object
         weatherData = JSON.parse(this.responseText);
-        console.log(weatherData);
-        // place object key/value into div
-        output.innerHTML = weatherData.current_observation.temp_f + "&deg;";
+        getLocation();
     }
     };
     // calls the open 
-    xhttp.open("GET", "http://api.wunderground.com/api/YouKey/geolookup/conditions/q/IA/Cedar_Rapids.json", true);
-    xhttp.send();
+    xhttp.open("GET", "http://api.wunderground.com/api/08946f7cb621e667/geolookup/conditions/q/MO/KANSAS_CITY.json", true);
+    xhttp.send();  
 }
+
+function hideText() {
+    
+}
+
+// get location
+function getLocation() {
+    // output div
+    locationDiv = document.getElementById('locationDiv');
+    console.log(weatherData);
+    // place object key/value into div
+    locationDiv.innerHTML = weatherData.current_observation.observation_location.full;    
+}
+
+// process request
+function getWeather() { 
+    // output div
+    output = document.getElementById('weather');
+    console.log(weatherData);
+    // place object key/value into div
+    document.getElementById('currentTempText').className = 'showText';
+    output.innerHTML = weatherData.current_observation.temp_f + "&deg;";
+}
+
 // eventlisteners 
 document.getElementById('getWeather').addEventListener('click', getWeather, false);
+window.addEventListener('load', startUp, false);
